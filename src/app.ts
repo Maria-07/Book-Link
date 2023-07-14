a1702a1b872d3bf0e9e5f480387023b1e14b89ef
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2023-07-14 11:13:47 +0600

    Server setup done and husky installed

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import httpStatus from 'http-status';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import routes from './app/routes';
+import cookieParser from 'cookie-parser';
+
+const app: Application = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  console.log('this is BookLink:Book-Catalog');
+  res.send('WELCOME TO BookLink 📖');
+});
+
+app.use('/api/v1/', routes);
+
+// global error handler
+app.use(globalErrorHandler);
+
+// Handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'Api not found',
+      },
+    ],
+  });
+  next();
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import config from './config';
+import { Server } from 'http';
+import app from './app';
+
+const uri = config.database_url as string;
+// console.log(uri);
+
+let server: Server;
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(uri);
+    console.log('🛢 database connection successful✨');
+
+    server = app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log('Failed to connect Database');
+  }
+  process.on('unhandledRejection', error => {
+    console.log('unhandledRejection is detected, we are closing our server');
+
+    if (server) {
+      server.close(() => {
+        console.log(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+bootstrap();
+
+process.on('SIGTERM', () => {
+  console.log('Sigterm is received');
+  if (server) {
+    server.close();
+  }
+});
